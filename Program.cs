22e7b567390c44ebf19d4fb22aa78690553da6f3
EvelynLoor <118677416+EvelynLoor@users.noreply.github.com>
2022-11-23 17:07:53 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+internal class Program
+{
+    public static void Main(string[]agrs){
+
+        GPS GPS1 = new GPS();
+        GPS1.imprimir();
+        CAPITAN capitan1 = new TRIPULANTE();
+        capitan1.bono();
+        capitan1.imprimir();
+        Jefe_de_Flota jFlota = TRIPULANTE();
+        jFlota.bono(1);
+        jFlota.imprimir();
+        Marinero marinero = TRIPULANTE();
+        jFlota.bono(1);
+        jFlota.imprimir();
+
+    }
+}
